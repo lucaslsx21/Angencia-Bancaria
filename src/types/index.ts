5a8f1ba2480f22dda10eb9b52ea3fe9d1d4fc5a1
Lucas Silva <lucaslsilva@live.com>
2025-04-14 20:36:47 -0300

    Não houve alterações

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,14 +9,14 @@ export interface Cliente {
   endereco: string;
   rendaAnual: number;
   patrimonio: number;
-  estadoCivil: "Solteiro" | "Casado" | "Viúvo" | "Divorciado";
+  estadoCivil: 'Solteiro' | 'Casado' | 'Viúvo' | 'Divorciado';
   codigoAgencia: number;
 }
 
 export interface Conta {
   id: string;
   cpfCnpjCliente: string;
-  tipo: "corrente" | "poupanca";
+  tipo: 'corrente' | 'poupanca';
   saldo: number;
   limiteCredito: number;
   creditoDisponivel: number;
